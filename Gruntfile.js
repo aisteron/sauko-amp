d45063b5dd668ffe5e885ac2146d0d201ad6a23a
unknown <timotheus@list.ru>
2018-06-28 23:40:40 +0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		pug: 
+			{
+			  compile: {
+			    options: {
+			      data: {
+			        debug: false
+			      }
+			    },
+			    
+			    files: {
+			      'index.html':['template/index.pug'],
+			    },
+			    
+			  }
+			},
+
+		less: 
+			{
+
+			  production: {
+			    options: 
+				    {
+					  
+					  paths: ['/template'],
+						//sourceMap:true,
+				      plugins: // временно отлючаю плагины, т.к. sourcemap не работает с ними.
+				      	[
+				        new (require('less-plugin-autoprefix'))({browsers: ["last 3 versions"]}),
+				        new (require('less-plugin-clean-css'))()
+				        ],
+				    },
+			    files: 
+				    {
+				      'style.css': 'template/style.less'
+				    }
+			  }
+			},	
+
+		watch:
+			{
+				css:
+				{
+					files:['template/*.less'],
+					tasks:['less'],
+					options: {
+				      livereload: true,
+				    }
+				},
+				pug:
+				{
+					files:['template/*.pug'],
+					tasks:['pug'],
+					options: {
+				      livereload: true,
+				    }
+				}
+			}   	
+
+	}); //end .initConfig
+
+	grunt.loadNpmTasks('grunt-contrib-pug');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+	grunt.registerTask('default',['pug','less', 'watch']);
+
+
+
+
+}; //end wrap
