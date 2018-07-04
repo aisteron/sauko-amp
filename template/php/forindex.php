87124cd2f313060362a1e402be68a8fdce3b7573
unknown <timotheus@list.ru>
2018-07-04 21:50:09 +0300

    h04.0721:49

--- template/php/forindex.php
+++ template/php/forindex.php
@@ -1,3 +1,4 @@
+<div class="exc_list">
 <?php 
 	$tours = new WP_Query(array('cat' => '3', 'posts_per_page'   => -1));
 
@@ -5,13 +6,13 @@
 		while($tours->have_posts()) : $tours->the_post(); 
 ?>
 
-<div class="listItem">
-	<div class="listImg">
+<div class="item">
+
 		<?php 
 		$value = get_field( "thumb" );
 		if($value) { ?>
 
-		<img class="lazyload" data-src="<?php the_field('thumb') ?>">
+		<amp-img src="<?php the_field('thumb') ?>" width="93" height="90" />
 		
 		<?php } else { ?>
 
@@ -20,26 +21,26 @@
 		<?php	} ?>
 
 
-	</div>
-	
-	<div class="listContent">
+	<div class="left">
 		<p><a href='<?php the_permalink();?>'><?php the_field('short-name'); ?></a></p>
-		<!--p><?php the_field('type'); ?></p-->
-		<p>
-			<span>от <span byn="<?php the_field('price'); ?>"><?php the_field('price'); ?></span> <span class="curSign">BYN</span></span>
-			<span><?php the_field('duration'); ?> ч</span>
-		</p>
+		<p class="price">от <?php the_field('price'); ?> р.</p>
 	</div>
+	<div class="right">
+		<p><?php the_field('duration'); ?> ч</p>
+	</div>	
 
-</div>
+</div> <!-- exc_list wrap-->
+
 
 <?php
 $counter++;
 endwhile;
 
 else :
-	echo 'smthng wrong! look tour.php';
+	echo 'smthng wrong! look twentytwelve/page-templates/amp/index.php';
 endif;
 wp_reset_postdata();	
 
 ?>
+
+</div>
